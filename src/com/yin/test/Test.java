ec51605fbd48d8f0b08ae3e50e177a83b563f8aa
yanbin.yin <yanbin.yin@qunar.com>
2015-11-10 11:20:12 +0800

    add test.java

---
+++ src/com/yin/test/Test.java
@@ -0,0 +1,10 @@
+package com.yin.test;
+
+/**
+ * Created by yanbin.yin on 2015/11/10.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello Git");
+    }
+}
